20a06baa994831cb8ecce7b630fc296fbe946bc8
Kim Laine <kim.laine@microsoft.com>
2020-01-06 21:58:00 -0800

    Implementing tabulation hashing

--- examples/example.cpp
+++ examples/example.cpp
@@ -4,6 +4,7 @@
 #include <kuku/kuku.h>
 #include <iostream>
 #include <iomanip>
+#include <chrono>
 
 using namespace std;
 using namespace kuku;
@@ -16,19 +17,19 @@ ostream &operator <<(ostream &stream, item_type item)
 
 void print_table(const KukuTable &table)
 {
-    size_t col_count = 8;
-    for (size_t row = 0; row < table.table_size() / col_count; row++)
+    table_size_type col_count = 8;
+    for (table_size_type row = 0; row < table.table_size() / col_count; row++)
     {
-        for (size_t col = 0; col < col_count; col++)
+        for (table_size_type col = 0; col < col_count; col++)
         {
-            size_t index = row * col_count + col;
+            table_size_type index = row * col_count + col;
             cout << setw(5) << index << ": " << setw(5) << table.table(index) << "\t";
         }
         cout << endl;
     }
 
     cout << endl << "Stash: " << endl;
-    for (size_t i = 0; i < table.stash().size(); i++)
+    for (table_size_type i = 0; i < table.stash().size(); i++)
     {
         cout << i << ": " << table.stash(i) << endl;
     }
@@ -37,76 +38,105 @@ void print_table(const KukuTable &table)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 5)
+    //if (argc != 5)
+    //{
+    //    cout << "Usage: ./example log_table_size stash_size loc_func_count max_probe" << endl;
+    //    cout << "E.g., ./example 8 2 4 100" << endl;
+
+    //    return 0;
+    //}
+
+    //int log_table_size = atoi(argv[1]);
+    //table_size_type stash_size = static_cast<size_t>(atoi(argv[2]));
+    //size_t loc_func_count = static_cast<size_t>(atoi(argv[3]));
+    //item_type loc_func_seed = make_random_item();
+    //uint64_t max_probe = static_cast<uint64_t>(atoi(argv[4]));
+    //item_type empty_item = make_item(0, 0);
+
+    //KukuTable table(
+    //    log_table_size,
+    //    stash_size,
+    //    loc_func_count,
+    //    loc_func_seed,
+    //    max_probe,
+    //    empty_item
+    //);
+
+    //uint64_t round_counter = 0;
+    //while (true)
+    //{
+    //    cout << "Inserted " << round_counter * 20 << " items" << endl;
+    //    cout << "Fill rate: " << table.fill_rate() << endl;
+
+    //    char c;
+    //    cin.get(c);
+
+    //    for (uint64_t i = 0; i < 20; i++)
+    //    {
+    //        if (!table.insert(make_item(i + 1, round_counter + 1)))
+    //        {
+    //            cout << "Insertion failed: round_counter = "
+    //                << round_counter << ", i = " << i << endl;
+    //            cout << "Inserted successfully " << round_counter * 20 + i << " items" << endl;
+    //            cout << "Fill rate: " << table.fill_rate() << endl;
+    //            cout << "Leftover item: " << table.last_insert_fail_item() << endl << endl;
+    //            break;
+    //        }
+    //    }
+
+    //    print_table(table);
+
+    //    if (!table.is_empty_item(table.last_insert_fail_item()))
+    //    {
+    //        break;
+    //    }
+
+    //    round_counter++;
+    //}
+
+    //while (true)
+    //{
+    //    cout << "Query item: ";
+    //    char hw[64];
+    //    char lw[64];
+    //    cin.getline(hw, 10, ' ');
+    //    cin.getline(lw, 10, '\n');
+    //    item_type item = make_item(
+    //        static_cast<uint64_t>(atoi(lw)), static_cast<uint64_t>(atoi(hw)));
+    //    QueryResult res = table.query(item);
+    //    cout << "Found: " << boolalpha << !!res << endl;
+    //    cout << "Location: " << res.location() << endl;
+    //    cout << "In stash: " << boolalpha << res.in_stash() << endl;
+    //    cout << "Hash function index: " << res.loc_func_index() << endl << endl;
+    //}
+
+    for (int i = 0; i < 10; i++)
     {
-        cout << "Usage: ./example log_table_size stash_size loc_func_count max_probe" << endl;
-        cout << "E.g., ./example 8 2 4 100" << endl;
+        int log_table_size = 21;
+        table_size_type stash_size = 0;
+        size_t loc_func_count = 3;
+        item_type loc_func_seed = make_random_item();
+        uint64_t max_probe = 50;
+        item_type empty_item = make_item(0, 0);
 
-        return 0;
-    }
+        KukuTable table(
+            log_table_size,
+            stash_size,
+            loc_func_count,
+            loc_func_seed,
+            max_probe,
+            empty_item
+        );
 
-    int log_table_size = atoi(argv[1]);
-    size_t stash_size = static_cast<size_t>(atoi(argv[2]));
-    size_t loc_func_count = static_cast<size_t>(atoi(argv[3]));
-    item_type loc_func_seed = make_random_item();
-    uint64_t max_probe = static_cast<uint64_t>(atoi(argv[4]));
-    item_type empty_item = make_item(0, 0);
-
-    KukuTable table(
-        log_table_size,
-        stash_size,
-        loc_func_count,
-        loc_func_seed,
-        max_probe,
-        empty_item
-    );
-
-    uint64_t round_counter = 0;
-    while (true)
-    {
-        cout << "Inserted " << round_counter * 20 << " items" << endl;
-        cout << "Fill rate: " << table.fill_rate() << endl;
-
-        char c;
-        cin.get(c);
-
-        for (uint64_t i = 0; i < 20; i++)
+        chrono::high_resolution_clock clk;
+        auto tp_start = clk.now();
+        for (uint64_t i = 0; i < (1 << 20); i++)
         {
-            if (!table.insert(make_item(i + 1, round_counter + 1)))
-            {
-                cout << "Insertion failed: round_counter = "
-                    << round_counter << ", i = " << i << endl;
-                cout << "Inserted successfully " << round_counter * 20 + i << " items" << endl;
-                cout << "Fill rate: " << table.fill_rate() << endl;
-                cout << "Leftover item: " << table.last_insert_fail_item() << endl << endl;
-                break;
-            }
+            table.insert(make_item(i + 1, i + 1));
         }
-
-        print_table(table);
-
-        if (!table.is_empty_item(table.last_insert_fail_item()))
-        {
-            break;
-        }
-
-        round_counter++;
-    }
-
-    while (true)
-    {
-        cout << "Query item: ";
-        char hw[64];
-        char lw[64];
-        cin.getline(hw, 10, ' ');
-        cin.getline(lw, 10, '\n');
-        item_type item = make_item(
-            static_cast<uint64_t>(atoi(lw)), static_cast<uint64_t>(atoi(hw)));
-        QueryResult res = table.query(item);
-        cout << "Found: " << boolalpha << !!res << endl;
-        cout << "Location: " << res.location() << endl;
-        cout << "In stash: " << boolalpha << res.in_stash() << endl;
-        cout << "Hash function index: " << res.loc_func_index() << endl << endl;
+        auto tp_end = clk.now();
+        auto tp_diff = chrono::duration_cast<chrono::milliseconds>(tp_end - tp_start);
+        cout << "Time: " << tp_diff.count() << " ms" << endl;
     }
 
     return 0;
--- src/kuku/locfunc.h
+++ src/kuku/locfunc.h
@@ -4,14 +4,7 @@
 #pragma once
 
 #include "kuku/common.h"
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable: 4804)
-#endif
-#include "kuku/blake2.h"
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+#include "kuku/hash.h"
 #include <cstring>
 #include <stdexcept>
 
@@ -20,7 +13,7 @@ namespace kuku
     class LocFunc
     {
     public:
-        LocFunc(int log_table_size, item_type seed) : seed_(seed)
+        LocFunc(int log_table_size, item_type seed) : hf_(seed)
         {
             if (log_table_size < 1 || log_table_size > max_log_table_size)
             {
@@ -35,28 +28,16 @@ namespace kuku
         LocFunc &operator =(const LocFunc &assign) = delete;
 
         /*
-        Returns an integer between 0 and (table_size-1).
+        Returns an integer between 0 and (table_size - 1).
         */
-        inline location_type operator ()(item_type item) const
+        inline location_type operator ()(item_type item) const noexcept
         {
-            return compress(item) & (table_size_ - 1);
+            return hf_(item) & (table_size_ - 1);
         }
 
     private:
-        inline location_type compress(item_type item) const
-        {
-            location_type out;
-            blake2b(
-                reinterpret_cast<unsigned char*>(&out),
-                sizeof(out),
-                reinterpret_cast<const unsigned char*>(item.data()),
-                sizeof(item),
-                reinterpret_cast<const unsigned char*>(seed_.data()), sizeof(seed_));
-            return out;
-        }
-
         table_size_type table_size_;
 
-        item_type seed_;
+        HashFunc hf_;
     };
 }
