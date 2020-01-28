35a54935558c36be13025906048cfbd97c4af8b9
Kim Laine <kim.laine@microsoft.com>
2020-01-27 18:37:58 -0800

    Fixed a bug in unit tests

--- tests/kuku/locfunc.cpp
+++ tests/kuku/locfunc.cpp
@@ -20,25 +20,24 @@ namespace kuku_tests
 
     TEST(LocFuncTests, Randomness)
     {
-        random_device rd;
-        for (int ts = min_table_size; ts < 5 * min_table_size; ts++)
+        for (table_size_type ts = min_table_size; ts < 5 * min_table_size; ts++)
         {
             for (int attempts = 0; attempts < 10; attempts++)
             {
-                uint64_t seed_lw = rd();
-                uint64_t seed_hw = rd();
-                LocFunc lf(ts, make_item(seed_lw, seed_hw));
-                LocFunc lf2(ts, make_item(seed_lw, seed_hw));
+                item_type seed = make_random_item();
+                LocFunc lf(ts, seed);
+                LocFunc lf2(ts, seed);
 
                 uint64_t zeros = 0;
                 uint64_t total = 1000;
                 for (uint64_t i = 0; i < total; i++)
                 {
-                    item_type bl = make_item(rd(), rd());
+                    item_type bl;
+                    set_random_item(bl);
                     ASSERT_TRUE(lf(bl) == lf2(bl));
                     zeros += (lf(bl) == size_t(0));
                 }
-                ASSERT_TRUE(abs(static_cast<double>(zeros) / static_cast<double>(total) - pow(2.0, -ts)) < 0.05);
+                ASSERT_TRUE(abs(static_cast<double>(zeros) / static_cast<double>(total) - 1/static_cast<double>(ts)) < 0.05);
             }
         }
     }
